9c2c66bc606206b950bd06b39125188219070304
raju15022004 <raju032004@gmail.com>
2026-08-25 23:26:35 +0530

    App.jsx

--- react_coder/src/App.jsx
+++ react_coder/src/App.jsx
@@ -1,115 +1,21 @@
 import { useState } from "react";
-// import CheckBoxes from "./CheckBoxes";
-// import User1 from "./User1";
 import Clock from "./Clock";
 
 
+
 function App(){
-
-// const userData=[
-//   {
-//     name:'Raju',
-//     age:'20',
-//     email:'raju032004gmail.com',
-//     id:1
-
-//   },
-
-//   {
-//     name:'Raju',
-//     age:'20',
-//     email:'raju032004gmail.com',
-//     id:2
-
-//   },
-
-//   {
-//     name:'Raju',
-//     age:'20',
-//     email:'raju032004gmail.com',
-//     id:3
-
-//   },
-
-//   {
-//     name:'Raju',
-//     age:'20',
-//     email:'raju032004gmail.com',
-//     id:4
-
-//   }
-// ]
-
-// const userData=[
-//   {
-//     name:'Raju',
-//     age:'20',
-//     email:"raju032002@gmail.com",
-//     id:1
-//   },
-//    {
-//     name:'Raju',
-//     age:'20',
-//     email:"raju032002@gmail.com",
-//     id:2
-//   },
-//    {
-//     name:'Raju',
-//     age:'20',
-//     email:"raju032002@gmail.com",
-//     id:3
-//   },
-//    {
-//     name:'Raju',
-//     age:'20',
-//     email:"raju032002@gmail.com",
-//     id:4
-//   }
-// ]
+ const[color,setColor]=useState('red');
   return(
-
-  //   <div>
-  // <h1>Loop in jxs with map Function</h1>
-  // <table border="1">
-  // <thead>
-  //   <tr>
-  //     <td>Id</td>
-  //     <td>Name</td>
-  //     <td>Email</td>
-  //     <td>Age</td>
-  //   </tr>
-  // </thead>
-  // <tbody>
-  //   {
-  //     userData.map((user)=>(
-  //       <tr>
-  //     <td>{user.id}</td>
-  //     <td>{user.name}</td>
-  //     <td>{user.email}</td>
-  //     <td>{user.age}</td>
-  //       </tr>
-
-  //     ))
-  //   }
-  // </tbody>
-  // </table>
-  //   </div>
-
-  // <div>
-  //   <h1>Reuse Component in Loop</h1>
-  //   {
-  //     userData.map((user)=>(
-  //       <div key={user.id}>
-  //        <User1  data={user}/>
-  //       </div>
-
-  //     ))
-  //   }
-  // </div>
 
   <div>
 <h1>Digital Clock in React js React Js</h1>
-<Clocl />
+<select onChange={(event)=>setColor(event.target.value)}>
+  <option value={"red"}>Red</option>
+  <option value={"blue"}>Blue</option>
+  <option value={"green"}>Green</option>
+  <option value={"orange"}>Orange</option>
+</select>
+<Clock  color={color} />
   </div>
   )
 }
--- react_coder/src/Clock.jsx
+++ react_coder/src/Clock.jsx
@@ -1,7 +1,17 @@
-const Clock=()=>{
+import { useEffect, useState } from "react"
+
+const Clock=({color})=>{
+  const [time,setTime]=useState("");
+
+  useEffect(()=>{
+    setInterval(()=>{
+      setTime(new Date().toLocaleTimeString());
+    },1000);
+  },[])
   return(
     <div>
-      <h1>Clock</h1>
+      <h1
+      style={{color:color,backgroundColor:'#000',width:'160px',padding:'10px',borderRadius:'10px'}}>{time}</h1>
     </div>
   )
 }
