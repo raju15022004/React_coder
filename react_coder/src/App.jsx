24140ea761ac3f3f5a902c238304d1ed2409b0a9
raju15022004 <raju032004@gmail.com>
2026-08-30 01:11:19 +0530

    App.jsx

--- react_coder/src/App.jsx
+++ react_coder/src/App.jsx
@@ -1,21 +1,34 @@
-let count=0
+import { useState } from "react";
+
 function App(){
 
+  const [users,setUsers]=useState([]);
+  const [user,setUser]=useState('');
+  const handleAddUsers=()=>{
+  setUsers([...users, user]);
+  }
+
+  const total=users.length;
+  const last=users[users.length-1];
+  const Unique=[...new Set(users)].length
+
   return(
-  <>
-  <h1>Keeping Components Pure</h1>
-  <Counter />
-  <Counter />
-  <Counter />
-  <Counter />
-    </>
-  );
+  <div>
+    <h2>Total User :{total}</h2>
+    <h2>Last User :{last}</h2>
+    <h2>Unique Total User:{Unique}</h2>
+
+   <input type="text" onChange={(event)=>setUser(event.target.value)} placeholder="add new user"/>
+   <button onClick={handleAddUsers}>Add User</button>
+
+{
+   users.map((item, index) => (
+  <h4 key={index}>{item}</h4>
+))
 }
 
-const Counter=()=>{
-  count=count+1
- return <h1>Counter {count}</h1>
+    </div>
+  )
 }
-
 
 export default App;
