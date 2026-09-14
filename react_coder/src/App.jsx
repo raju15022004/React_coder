b5c4b56c002a23735ce7f63f08a60ed603b27602
raju15022004 <raju032004@gmail.com>
2026-09-15 00:04:23 +0530

    App.jsx

--- react_coder/src/App.jsx
+++ react_coder/src/App.jsx
@@ -1,12 +1,40 @@
-import { useState } from "react";
+import { useActionState } from "react";
 
-function App(){
-const [name,setName]=useState("Raju")
+ function App(){
+  const handleSubmit=async(previousData,FormData)=>{
+    let name=FormData.get('name');
+    let password=FormData.get('password');
+
+    await new Promise(res=>setTimeout(res,2000))
+    if(name && password){
+      return{message:'Data Submitted'}
+    }else{
+      return{error:'Failed to Submit.Enter proper data'}
+    }
+
+    // console.log("handleSubmit called",name,password);
+  }
+
+  const [data,action,pending]=useActionState(handleSubmit,undefined)
+   console.log(data);
   return(
   <div>
-  <h1>Updating Array in State</h1>
-  <h2>{name}</h2>
-  <button onClick={()=>setName("Raju Kumar")}>Update Name</button>
+    <h1>useActionState Hook in React js</h1>
+    <form action={action}>
+      <input type="text" placeholder="enter name" name="name"/>
+      <br/><br/>
+      <input type="password" placeholder="enter password" name="password"/>
+      <br/><br/>
+      <button disabled={pending}>Submit Data</button>
+      <br/>
+      {
+        data?.error && <span style={{color:'red'}}>{data?.error}</span>
+      }
+      {
+        data?.message && <span style={{color:"green"}}>{data?.message}</span>
+      }
+    </form>
+
     </div>
   )
 }
