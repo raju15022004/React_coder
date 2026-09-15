94396b877c1792a07812fd4294188bdfb84eb90f
raju15022004 <raju032004@gmail.com>
2026-09-15 18:19:03 +0530

    useToggle.jsx

--- react_coder/src/App.jsx
+++ react_coder/src/App.jsx
@@ -1,41 +1,22 @@
-import { useActionState, useId } from "react";
-
+import useToggle from "./useToggle"
  function App(){
 
+  const[value,toggleValue]=useToggle(true)
+
+  // console.log("val----",value);
   return(
   <div>
-  <UserForm/>
+    <button onClick={toggleValue}>Toggle Heading</button>
+    <button onClick={()=>toggleValue(false)}>Hide Heading</button>
+    <button onClick={()=>toggleValue(true)}>Show Heading</button>
 
-    </div>
+    {
+      value?<h1>Custom in React js</h1>:null
+    }
+  </div>
   )
 }
 
-function UserForm(){
-  const name=useId();
-  const password=useId();
-  const terms=useId();
-  const skills=useId();
 
 
-return(
-  <div>
-    <form action="">
-      <lable htmlFor={name}>ENter User Name</lable>
-      <input id={name} type="text" placeholder="enter name"/>
-      <br/><br/>
-      <lable htmlFor={password}>ENter User password</lable>
-      <input id={password} type="text" placeholder="enter password"/>
-      <br/><br/>
-      <lable htmlFor={skills}>ENter User skills</lable>
-      <input id={skills} type="text" placeholder="enter skills"/>
-      <br/><br/>
-      <input id={terms} type="checkbox" placeholder="enter terms"/>
-      <lable htmlFor={terms}>Terms and Condition</lable>
-
-      <br/><br/>
-    </form>
-  </div>
-
-)
-}
 export default App;
