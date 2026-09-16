d51d8dafe89a0636cbd11195129356c972673859
raju15022004 <raju032004@gmail.com>
2026-09-16 13:32:02 +0530

    header.jsx

--- react_coder/src/NavBar.jsx
+++ react_coder/src/NavBar.jsx
@@ -1,11 +1,29 @@
 import { Link } from "react-router";
+import './header.css'
 function NavBar(){
+
   return(
-    <div>
-<Link to="/"><h1>Home</h1></Link>
-<Link to="/about"><h1>About</h1></Link>
-<Link to="/login"><h1>Login</h1></Link>
+    <div className="header">
+      <div>
+        <Link className="link"><h2>Logo</h2></Link>
+      </div>
+
+      <div>
+        <ul>
+          <li>
+            <Link className="link" to="/">Home</Link>
+          </li>
+          <li>
+            <Link className="link" to="/login">Login</Link>
+          </li>
+          <li>
+            <Link className="link" to="/about">About</Link>
+          </li>
+        </ul>
+      </div>
     </div>
+
+
   )
 }
 
