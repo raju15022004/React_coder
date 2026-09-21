f1bc6c10030fa30f4d5d54c1d779a6320e2d9abb
raju15022004 <raju032004@gmail.com>
2026-09-21 22:06:57 +0530

    userEdit.jsx

--- react_coder/src/UserEdit.jsx
+++ react_coder/src/UserEdit.jsx
@@ -1,13 +1,38 @@
+import { useEffect, useState } from "react";
+import {useParams} from "react-router";
+
 function UserEdit(){
+  const {id}=useParams();
+  const [name,setName]=useState('')
+  const [age,setAge]=useState('')
+  const [email,setEmail]=useState('')
+
+
+  useEffect(()=>{
+    getUserData()
+  },[])
+
+  const getUserData=async()=>{
+    const url="http://localhost:3000/users/"+id;
+    let response=await fetch(url);
+    response =await response.json();
+
+    console.log(response);
+    setName(response.name)
+    setAge(response.age)
+    setEmail(response.email)
+  }
+
   return(
     <div style={{textAlign:'center'}}>
       <h1>Edit User Details </h1>
-      <input type="text" placeholder="enter name"/>
+      <input type="text" value={name} placeholder="user name"/>
       <br/><br/>
-      <input type="text" placeholder="enter age"/>
+      <input type="text" value={age} placeholder="user age"/>
       <br/><br/>
-      <input type="text" placeholder="enter email"/>
+      <input type="text" value={email} placeholder="user email"/>
       <br/><br/>
+      <button >Update User</button>
     </div>
   )
 }
