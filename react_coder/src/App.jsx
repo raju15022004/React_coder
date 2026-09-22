04364a53f935469bd013766fb40e88af263f14a3
raju15022004 <raju032004@gmail.com>
2026-09-22 14:21:18 +0530

    App.jsx

--- react_coder/src/App.jsx
+++ react_coder/src/App.jsx
@@ -1,29 +1,45 @@
-import {Routes,Route, NavLink} from 'react-router'
+// import {Routes,Route, NavLink} from 'react-router'
+import { useState } from 'react';
 import './App.css'
-import UserList from "./UserList";
-import UserAdd from './UserAdd';
-import UserEdit from './UserEdit';
+
 
 
 function App() {
 
+  const [name,setName]=useState('');
+  const [nameErr,setNameErr]=useState();
 
+  const [password,setPassword]=useState('');
+  const [passErr,setPassErr]=useState();
+
+  const handleName=(event)=>{
+    console.log(event.target.value);
+    if(event.target.value.length>5){
+     setNameErr("Please enter valid username. only 5 characters allowed")
+    }else{
+      setNameErr()
+    }
+  }
+
+  const handlPassword=(event)=>{
+    let regex=/^[A-Z0-9]+$/i;
+    if(regex.test (event.target.value)){
+      setPassErr()
+    }else{
+     setPassErr("Please enter valid password. only number and alphabets allowed")
+
+    }
+  }
 
   return (
-<div className=' user-list-head'>
-  <ul className='nav-list'>
-    <li>
-    <NavLink to="/">List</NavLink>
-    </li>
-    <li>
-      <NavLink to="/add">Add User</NavLink>
-    </li>
-  </ul>
-<Routes>
-  <Route path="/" element={<UserList/>} />
-  <Route path="/add" element={<UserAdd/>} />
-  <Route path="/edit/:id" element={<UserEdit/>} />
-</Routes>
+<div>
+<input className={nameErr?'error':''} type='text' onChange={handleName} placeholder='enter name'/>
+<span className='red-color'>{nameErr && nameErr}</span>
+<br/><br/>
+<input className={passErr?'error':''}  type="text"  onChange={handlPassword} placeholder='enter password' />
+<span className='red-color'>{passErr && passErr}</span>
+<br/><br/>
+<button disabled={passErr || nameErr}>Login</button>
 </div>
   );
 }
